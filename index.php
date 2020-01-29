91d693f418c8ea2ccd767ac98042192d788e8778
Goshgar <qosqarmirze@gmail.com>
2020-01-29 12:08:03 +0400

    Tests

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<input type="text" autofocus />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+<script>
+$('#input').focus();
+$('#input').trigger('click');
+
+</script>
+<input type="text" autofocus id="input" />
