fff057d71e7890e060100d19b0c783f39c0a7792
Goshgar <qosqarmirze@gmail.com>
2020-01-29 12:13:02 +0400

    Tests

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
 <script>
+$(document).ready(function(){
 $('#input').focus();
-$('#input').trigger('click');
+$('#input').trigger('click');	
+})
+
 
 </script>
 <input type="text" id="input" />
